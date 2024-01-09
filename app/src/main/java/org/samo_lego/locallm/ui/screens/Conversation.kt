8d121690ae5090ad8b673d0b69a7f7e4103e10cf
samolego <34912839+samolego@users.noreply.github.com>
2024-01-09 18:15:06 +0100

    Usable (but ugly) chat ui

--- app/src/main/java/org/samo_lego/locallm/ui/screens/Conversation.kt
+++ app/src/main/java/org/samo_lego/locallm/ui/screens/Conversation.kt
@@ -1,41 +1,76 @@
 package org.samo_lego.locallm.ui.screens
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.samo_lego.locallm.lmloader.LMHolder
+import org.samo_lego.locallm.ui.components.BotMessage
 import org.samo_lego.locallm.ui.components.Input
 import org.samo_lego.locallm.ui.components.TextResponse
+import org.samo_lego.locallm.ui.components.UserMessage
 
 private val messages: MutableList<TextResponse> = mutableStateListOf()
 
+@Preview(showBackground = true)
 @Composable
 fun Conversation() {
-    Row {
-        LazyColumn(modifier = Modifier.fillMaxHeight()) {
-            items(messages) { message ->
-                Column {
-                    if (message.isBot) {
-                        TextResponse.Bot(message.message)
-                    } else {
-                        TextResponse.User(message.message)
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color.Transparent) // Replace with your styling for message history
+        ) {
+
+            LazyColumn {
+                items(messages) { message ->
+                    Column {
+                        message.Render()
                     }
                 }
             }
         }
-        Column {
-            Input(onTextSend = {
-                val text = it.trim()
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxWidth()
+                .background(Color.White)
+        ) {
+            Input(
+                onTextSend = {
+                    val text = it.trim()
 
-                if (text.isNotEmpty()) {
-                    // Add new text reponse to view
-                    messages.add(TextResponse(false, text))
-                }
-            })
+                    if (text.isNotEmpty()) {
+                        // Add new text response to view
+                        messages.add(UserMessage(text))
+
+
+                        val botTokens = mutableStateListOf<String>()
+                        val botResponse = BotMessage(botTokens)
+                        messages.add(botResponse)
+
+                        // Run model to generate response
+                        LMHolder.suggest(text, onSuggestion = { suggestion ->
+                            Log.v("LocalLM", "Suggestion: $suggestion")
+                            botTokens.add(suggestion)
+                        })
+                    }
+                },
+            )
         }
     }
 }
