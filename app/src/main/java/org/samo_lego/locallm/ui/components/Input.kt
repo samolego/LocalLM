c30c33a89cd66851f3e29aefdf0dd5fba26fe610
samolego <34912839+samolego@users.noreply.github.com>
2024-02-05 19:30:34 +0100

    Add ability to stop text generation

--- app/src/main/java/org/samo_lego/locallm/ui/components/Input.kt
+++ app/src/main/java/org/samo_lego/locallm/ui/components/Input.kt
@@ -2,6 +2,7 @@ package org.samo_lego.locallm.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,12 +15,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview
 @Composable
-fun Input(onTextSend: (String) -> Unit = {}) {
+fun Input(
+    isGenerating: Boolean,
+    onTextSend: (String) -> Unit = {},
+    onForceStopGeneration: () -> Unit = {}
+) {
     var text by rememberSaveable {
         mutableStateOf("")
     }
+
     TextField(
         modifier = Modifier.fillMaxWidth(),
         value = text,
@@ -27,15 +32,34 @@ fun Input(onTextSend: (String) -> Unit = {}) {
         trailingIcon = {
             IconButton(
                 onClick = {
-                    onTextSend(text)
-                    text = ""
+                    if (isGenerating) {
+                        onForceStopGeneration()
+                    }
+
+                    if (text.isNotEmpty()) {
+                        onTextSend(text)
+                        text = ""
+                    }
                 },
             ) {
                 Icon(
-                    Icons.Filled.Send,
+                    if (isGenerating) Icons.Default.Close else Icons.Default.Send,
                     contentDescription = "Send",
                 )
             }
         },
     )
 }
+
+@Preview
+@Composable
+fun InputPreview() {
+    Input(isGenerating = false)
+}
+
+@Preview
+@Composable
+fun InputPreviewGenerating() {
+    Input(isGenerating = true)
+}
+
--- app/src/main/java/org/samo_lego/locallm/ui/screens/Conversation.kt
+++ app/src/main/java/org/samo_lego/locallm/ui/screens/Conversation.kt
@@ -10,8 +10,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,6 +44,11 @@ fun Conversation() {
     val keyboardController = LocalSoftwareKeyboardController.current
     val scrollState = rememberLazyListState()
 
+    var allowGenerating by remember { mutableStateOf(false) }
+    var botResponse by remember {
+        mutableStateOf(BotMessage())
+    }
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -47,7 +56,7 @@ fun Conversation() {
             modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth()
-                .background(Color.Transparent) // Replace with your styling for message history
+                .background(Color.Transparent)
         ) {
             LazyColumn(
                 state = scrollState,
@@ -60,8 +69,9 @@ fun Conversation() {
             }
         }
         Input(
-            onTextSend = {
-                val text = it.trim()
+            isGenerating = allowGenerating,
+            onTextSend = { str ->
+                val text = str.trim()
 
                 if (text.isNotEmpty()) {
                     // Hide keyboard
@@ -70,15 +80,19 @@ fun Conversation() {
                     // Add new text response to view
                     messages.add(UserMessage(text))
 
-                    var botResponse = BotMessage()
                     messages.add(botResponse)
 
                     // Run model to generate response
                     tts.reset()
+                    allowGenerating = true
                     LMHolder.suggest(text, onSuggestion = { suggestion ->
+                        if (!allowGenerating) {
+                            return@suggest false
+                        }
                         Log.v("LocalLM", "Suggestion: $suggestion")
                         if (suggestion.isEmpty()) {
                             onEndSuggestions(botResponse.tokens.value, botResponse, ttScope)
+                            allowGenerating = false
 
                             return@suggest false
                         } else {
@@ -86,12 +100,14 @@ fun Conversation() {
 
                             if (botResponse.tokens.value.contains(ChatMLUtil.im_end)) {
                                 onEndSuggestions(botResponse.tokens.value, botResponse, ttScope)
+                                allowGenerating = false
                                 return@suggest false
                             }
 
                             backticks += suggestion.count { it == '`' }
 
                             if (botResponse.tokens.value.endsWith("\n\n") && backticks % 2 == 0) {
+                                // New bot message bubble
                                 botResponse = BotMessage()
                                 messages.add(botResponse)
                             }
@@ -107,11 +123,19 @@ fun Conversation() {
                         }
 
                         return@suggest true
-                    }, onEnd = {
-                        onEndSuggestions(botResponse.tokens.value, botResponse, ttScope)
-                    })
+                    },
+                        onEnd = {
+                            onEndSuggestions(botResponse.tokens.value, botResponse, ttScope)
+                            allowGenerating = false
+                        })
                 }
             },
+            onForceStopGeneration = {
+                allowGenerating = false
+                tts.reset()
+                botResponse.appendToken(" ... [-- Stopped --]")
+                botResponse = BotMessage()
+            }
         )
     }
 }
